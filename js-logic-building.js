932949f1af2a1b50b3bc858b50a4a5c29d520f89
AbbasSherazi89 <ibne.abbassherazi89@gmail.com>
2025-04-16 11:10:45 +0500

    Factorial of a number

---
+++ js-logic-building.js
@@ -0,0 +1,10 @@
+// Factorial of a given number
+
+const factorialOfnum = (num) => {
+  let fact = 1;
+  for (let i = num; i >= 1; i--) {
+    fact *= i;
+  }
+  return fact;
+};
+console.log(factorialOfnum(5));

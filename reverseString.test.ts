e1eb3ccf3dc68a6359a85708f66c8b0a64d3a457
AbbasSherazi89 <ibne.abbassherazi89@gmail.com>
2025-04-16 11:55:36 +0500

    reverse of a string

---
+++ reverseString.test.ts
@@ -0,0 +1,8 @@
+const reverseString = require("./reverseString");
+
+console.log(
+  reverseString("hello") === "olleh" ? "✅ Test passed" : "❌ Test failed"
+);
+console.log(
+  reverseString("Uplift") === "tfilpU" ? "✅ Test passed" : "❌ Test failed"
+);

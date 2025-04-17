f30e368d79c4063f7708bd4e2b6271c388f2131c
AbbasSherazi89 <ibne.abbassherazi89@gmail.com>
2025-04-17 14:25:15 +0500

    Check if a string is a palindrome

--- js-logic-building.js
+++ js-logic-building.js
@@ -15,3 +15,16 @@ const findMaxNum=(arr)=>{
 }
 
 console.log("Maximum number in the array is: ", findMaxNum([23,44,33,90,44,33,22,3,87]));
+
+// Check if a string is a palindrome.
+let str="MOm";
+const findPalindrome=(word)=>{
+  let str1=word.split("").join("").toLowerCase();  
+  let str2=word.split("").reverse().join("").toLowerCase();
+  if(str1===str2){
+    console.log(word, "is palindrome");
+  }else{
+    console.log(word, "is not a palindrome")
+  }
+}
+console.log(findPalindrome(str));
